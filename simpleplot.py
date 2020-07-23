05e97516bcd933d47b34b8699256c2038b0a3ab1
Kaveh Pezeshki <kaveh@pezeshki.org>
2020-07-23 09:47:48 -0700

    updated two-dimensional plots with directivity

--- simpleplot.py
+++ simpleplot.py
@@ -1,65 +1,96 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import csvinterface
+import math
 
+def degtorad(val):
+    return val*math.pi/180
+
+def translatecoords(angle, intensity):
+    xval = intensity * math.cos(degtorad(angle))
+    yval = intensity * math.sin(degtorad(angle))
+    return xval, yval
 
 def simple_plot(means_filename, stdev_filename):
 	#graph 1
-	
-	xData, yData, zData = csvinterface.read_csv(means_filename)
-	xData2, yData2, zData2 = csvinterface.read_csv(stdev_filename)
 
-	x1 = yData
-	x2 = yData
-	x3 = yData2
-	x4 = yData2
+        baseAngle, armAngle, intensitiesMean = csvinterface.read_csv(means_filename)
+        baseAngle, armAngle, intensitiesStdev = csvinterface.read_csv(means_filename)
 
-	y1 = zData[0]
-	y2 = zData[1]
-	y3 = zData2[0]
-	y4 = zData2[1]
+        intensityMean0 = intensitiesMean[0]
+        intensityMean90 = intensitiesMean[1]
+        intensityStdev0 = intensitiesStdev[0]
+        intensityStdev90 = intensitiesStdev[1]
+
+        directivity_mean_0_x = []
+        directivity_mean_0_y = []
+        directivity_mean_90_x = []
+        directivity_mean_90_y = []
+
+        for angle, intensity in zip(armAngle, intensityMean0):
+            x,y = translatecoords(angle, intensity)
+            directivity_mean_0_x.append(x)
+            directivity_mean_0_y.append(y)
+
+        for angle, intensity in zip(armAngle, intensityMean90):
+            x,y = translatecoords(angle, intensity)
+            directivity_mean_90_x.append(x)
+            directivity_mean_90_y.append(y)
+
+        snr0 = []
+        snr90 = []
+
+        for val,err in zip(intensityMean0, intensityStdev0):
+            snr0.append(20*math.log10(val/err))
+
+        for val,err in zip(intensityMean90, intensityStdev90):
+            snr90.append(20*math.log10(val/err))
 
 	plt.subplot(1, 2, 1)
-	plt.plot(x1, y1, '.-')
-	plt.plot(x2, y2, '.-')
-	plt.title('Average Value')
-	plt.xlabel('Phi')
-	plt.ylabel('Voltage')
-	plt.legend( [ '0 Deg', '90 Deg'] )
-	
+	plt.plot(directivity_mean_0_x, directivity_mean_0_y, '.-')
+	plt.plot(directivity_mean_90_x, directivity_mean_90_y, '.-')
+	plt.title('Mean Directivity')
+        plt.legend( [ 'Base: 0 Deg', 'Base: 90 Deg'] )
+
 	plt.subplot(1, 2, 2)
-	plt.plot(x3, y3, '.-')
-	plt.plot(x4, y4, '.-')
-	plt.title('Standard Deviation')
-	plt.xlabel('Phi')
-	plt.ylabel('Voltage')
-	plt.legend( [ '0 Deg', '90 Deg'] )
+	plt.plot(armAngle, snr0, '.-')
+	plt.plot(armAngle, snr1, '.-')
+	plt.title('Signal to Noise Ratio')
+	plt.xlabel('Arm Angle')
+	plt.ylabel('SNR (dB, approx from σ)')
+        plt.legend( [ 'Base: 0 Deg', 'Base: 90 Deg'] )
 	plt.show()
 
 def one_axis_plot(means_filename, stdev_filename):
 
-	xData, yData, zData = csvinterface.read_csv(means_filename)
-	xData2, yData2, zData2 = csvinterface.read_csv(stdev_filename)
+	baseAngle, armAngle, intensityMean = csvinterface.read_csv(means_filename)
+	baseAngle, armAngle, intensityStdev = csvinterface.read_csv(stdev_filename)
 
-	x1 = yData
-	x2 = yData2
-	
-	y1 = zData[0]
-	y2 = zData2[0]
+
+        directivity_mean_x = []
+        directivity_mean_y = []
+
+        for angle, intensity in zip(armAngle, intensityMean):
+            x, y = translatecoords(angle, intensity)
+            directivity_mean_x.append(x)
+            directivity_mean_y.append(y)
+
+        snr = []
+
+        for val, err in zip(intensityMean, intensityStdev):
+            snr.append(20*math.log10(val/err))
 
 	plt.subplot(1, 2, 1)
-	plt.plot(x1, y1, '.-')
-	plt.title('Average Value')
-	plt.xlabel('Phi')
-	plt.ylabel('Voltage')
-	plt.legend( [ '0 Deg'] )
-	
+	plt.plot(directivity_mean_x, directivity_mean_y, '.-')
+	plt.title('Mean Directivity')
+        plt.legend( [ 'Base: 0 Deg'] )
+
 	plt.subplot(1, 2, 2)
-	plt.plot(x2, y2, '.-')
-	plt.title('Standard Deviation')
-	plt.xlabel('Phi')
-	plt.ylabel('Voltage')
-	plt.legend( [ '0 Deg'] )
+	plt.plot(armAngle, snr, '.-')
+	plt.title('Signal to Noise Ratio')
+	plt.xlabel('Arm Angle')
+	plt.ylabel('SNR (dB, approx from σ)')
+        plt.legend( [ 'Basee: 0 Deg'] )
 	plt.show()
 
 #one_axis_plot("farfield_0_means.csv", "farfield_0_stdevs.csv")
