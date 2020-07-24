37b85af8908f57c9cfd7ea8eeab9f727a9e18cd8
Kaveh Pezeshki <kaveh@pezeshki.org>
2020-07-24 16:59:21 -0700

    added pretty mesh plotting

--- twographs.py
+++ twographs.py
@@ -2,8 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.tri as mtri
 from matplotlib import cm
+from mpl_toolkits.mplot3d import Axes3D
 import csvinterface
 import math
+from mayavi import mlab
+#import numpy as np
 
 
 def to_rad(val):
@@ -25,8 +28,8 @@ def transform_val(theta, phi, intensity): # theta phi swapped
 
         return xpos, ypos, zpos
 
-
-def plot_surface_file(ax, filename, scatter = True):
+def plot_mayavi(filename):
+    
         xData, yData, zData = csvinterface.read_csv(filename)
         
         # assume constant baseline and linear scaling - subtract off mininum value
@@ -36,8 +39,69 @@ def plot_surface_file(ax, filename, scatter = True):
         for row in zData:
             for el in row:
                 if el < minval: minval = el
+
+        corrected_x_data = []
+        corrected_y_data = []
+        corrected_z_data = []
         
-        print("minval", minval)
+
+        for phiindex in range(len(zData)):
+                phirow = zData[phiindex]
+                for thetaindex in range(len(phirow)):
+
+                        intensity = phirow[thetaindex]
+                        phi = xData[phiindex]
+                        theta = yData[thetaindex]
+
+                        x, y, z = transform_val(phi, theta, intensity-minval)
+                        
+
+                        corrected_x_data.append(x)
+                        corrected_y_data.append(y)
+                        corrected_z_data.append(z)
+                         
+        # generating a triangular mesh
+            
+        num_phipoints = len(zData[0])
+        
+        # points are theta, phi 0-180
+        # first 91 points for theta = 0, next for theta = 1, etc
+        
+        # connect points in consecutive theta
+        
+        triangles = []
+        
+        # there will be as many triangles as there are points - 2
+        
+        for theta_index in range(len(zData)-1):
+            for phi_index in range(len(zData[0])-1):
+                # indices will be two consecutive points for theta = base, then one point for theta = base + 1
+                first_point_index  = theta_index*num_phipoints + phi_index
+                second_point_index = theta_index*num_phipoints + phi_index + 1
+                third_point_index  = (theta_index+1)*num_phipoints + phi_index
+                fourth_point_index = (theta_index+1)*num_phipoints + phi_index + 1
+                                
+                triangles.append([first_point_index, second_point_index, third_point_index])
+                triangles.append([fourth_point_index, second_point_index, third_point_index])
+        
+        #mlab.options.backend = 'envisage'
+        mlab.figure("Mean Directivity")
+        mlab.triangular_mesh(corrected_x_data, corrected_y_data, corrected_z_data, triangles)   
+        mlab.axes()
+        mlab.show()
+
+
+
+def plot_surface_file(ax, filename, scatter = False):
+        xData, yData, zData = csvinterface.read_csv(filename)
+        
+        # assume constant baseline and linear scaling - subtract off mininum value
+        
+        minval = 10 # max of 5v on ADC, so this is absurdly high
+        
+        for row in zData:
+            for el in row:
+                if el < minval: minval = el
 
         corrected_x_data = []
         corrected_y_data = []
@@ -59,32 +123,51 @@ def plot_surface_file(ax, filename, scatter = True):
                         corrected_y_data.append(y)
                         corrected_z_data.append(z)
                     
-
-
-        # Plot the surface.
+        
 
         if scatter:
-            surf = ax.scatter(corrected_x_data, corrected_y_data, corrected_z_data, marker = '.', c=corrected_z_data, s=20, alpha=1)
+            surf = ax.scatter(corrected_x_data, corrected_y_data, corrected_z_data, marker = '.', c = corrected_z_data, s=20, alpha=1)
             #ax.contourf(corrected_x_data, corrected_y_data, corrected_z_data)
         else: 
-            triang = mtri.Triangulation(corrected_x_data, corrected_y_data)
-
-            # Mask off unwanted triangles.
-            xmid = np.array(corrected_x_data)[triang.triangles].mean(axis=1)
-            ymid = np.array(corrected_y_data)[triang.triangles].mean(axis=1)
-            mask = xmid**2 + ymid**2 < 0.1**2
-            triang.set_mask(mask)
-
-            #surf = ax.plot_trisurf(np.array(corrected_x_data), np.array(corrected_y_data), np.array(corrected_z_data), cmap=cm.viridis, linewidth=0, antialiased=False)
-            surf = ax.plot_trisurf(triang, np.array(corrected_z_data), cmap=cm.viridis, linewidth=0, antialiased=False)
-
+                        
+            # generating a triangular mesh
+                
+            num_phipoints = len(zData[0])
+            
+            # points are theta, phi 0-180
+            # first 91 points for theta = 0, next for theta = 1, etc
+            
+            # connect points in consecutive theta
+            
+            triangles = []
+            
+            # there will be as many triangles as there are points - 2
+            
+            for theta_index in range(len(zData)-1):
+                for phi_index in range(len(zData[0])-1):
+                    # indices will be two consecutive points for theta = base, then one point for theta = base + 1
+                    first_point_index  = theta_index*num_phipoints + phi_index
+                    second_point_index = theta_index*num_phipoints + phi_index + 1
+                    third_point_index  = (theta_index+1)*num_phipoints + phi_index
+                    fourth_point_index = (theta_index+1)*num_phipoints + phi_index + 1
+                                        
+                    triangles.append([first_point_index, second_point_index, third_point_index])
+                    triangles.append([fourth_point_index, second_point_index, third_point_index])
+            
+            triang = mtri.Triangulation(corrected_x_data, corrected_y_data, triangles=triangles)
+    
+            surf = ax.plot_trisurf(triang, np.array(corrected_z_data), linewidths=0.1, edgecolor = 'black', cmap=cm.viridis, antialiased=True)
             
 
         return surf, zData
 
 
 def plot_everything(means_filename, stdev_filename):
+    
+        print("Plotting in Mayavi")
+        plot_mayavi("farfield_3_means.csv")
 
+        print("Plotting in Matplotlib")
         #set up a figure
         fig = plt.figure(figsize=plt.figaspect(0.3))
 
@@ -101,9 +184,9 @@ def plot_everything(means_filename, stdev_filename):
         # Tweaking display region and labels
         #ax1.set_xlim(0, 180)
         #ax1.set_ylim(0, 180)
-        ax1.set_xlabel('Base', color='crimson')
-        ax1.set_ylabel('Base', color='crimson')
-        ax1.set_zlabel('Z Magnitude', color='crimson')
+        #ax1.set_xlabel('Base', color='crimson')
+        #ax1.set_ylabel('Base', color='crimson')
+        #ax1.set_zlabel('Z Magnitude', color='crimson')
 
         # Customize the z axis.
         #ax1.set_zlim(0, 5)
@@ -128,8 +211,7 @@ def plot_everything(means_filename, stdev_filename):
 
         # Plot the surface.
         ax2 = fig.gca(projection='3d')
-        surf = ax2.plot_surface(xData2, yData2, zData2, cmap=cm.viridis,
-                               linewidth=0, antialiased=False)
+        surf = ax2.plot_surface(xData2, yData2, zData2, cmap=cm.viridis, linewidths=0.1, edgecolor = 'black', antialiased=True)
 
         # Title
         # Placement 0, 0 would be the bottom left, 1, 1 would be the top right.
@@ -153,6 +235,6 @@ def plot_everything(means_filename, stdev_filename):
 
         plt.show()
 
-#plot_everything("farfield_3_means.csv", "farfield_3_stdevs.csv")
+plot_everything("farfield_3_means.csv", "farfield_3_stdevs.csv")
 
 
